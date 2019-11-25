357df80f3b9c61d26d92e951903fa3fa95f85088
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-11-24 22:05:49 -0800

    feat: Add source line for nvm command to work

--- setup/node.sh
+++ setup/node.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# Enable nvm
+source /usr/local/opt/nvm/nvm.sh
+
+# Install node via nvm, then check to make sure it's the right version
 nvm install node
-command -v nvm
+which node
